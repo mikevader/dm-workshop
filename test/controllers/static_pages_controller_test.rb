9b3e67e6076a2ed006abdc513dc28b1c57089da0
Michael Mühlebach <michael@anduin.ch>
2015-08-19 22:26:32 +0200

    Add a full_title helper and improved testing behaviour.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Dungeon Masters Workshop"
+  end
+    
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Dungeon Masters Workshop"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Dungeon Masters Workshop"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Dungeon Masters Workshop"
+    assert_select "title", "About | #{@base_title}"
   end
 end
