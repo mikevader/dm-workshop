3c8167fd04d02be1c84632dbbaa209f03ebd4702
Michael Mühlebach <michael@anduin.ch>
2015-12-26 13:53:30 +0100

    Remove cards subtree.

--- cards/cards.rb
+++
@@ -1,9 +0,0 @@
-require 'nokogiri'
-
-document = Nokogiri::XML(File.read('monsters.xml'))
-template = Nokogiri::XSLT(File.read('template.xsl'))
-
-transformed_document = template.transform(document)
-
-File.open('output.html', 'w').write(transformed_document)
-
