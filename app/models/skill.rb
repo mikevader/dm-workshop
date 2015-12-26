6e04c27c4fd8b0c86e8e88d997a028fc837182ce
Michael Mühlebach <michael@anduin.ch>
2015-12-26 14:35:49 +0100

    Add uniqueness to skill name.

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
   has_and_belongs_to_many :monsters
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :ability, presence: true
 end
