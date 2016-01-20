ef718273149792d423e00c7385af5d51349694d9
Michael Mühlebach <michael@anduin.ch>
2016-01-20 07:53:28 +0100

    Fix uniqueness and order of spell classes.

--- app/models/hero_class.rb
+++ app/models/hero_class.rb
@@ -1,6 +1,8 @@
 class HeroClass < ActiveRecord::Base
   has_and_belongs_to_many :spells, -> { distinct }, uniq: true, order: name
 
+  default_scope -> { order(name: :asc) }
+
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :cssclass, presence: true, length: { maximum: 50 }
 end
