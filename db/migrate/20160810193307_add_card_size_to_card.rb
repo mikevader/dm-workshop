76d42b709aef9d88f78b1d8542cf8074697bf788
Michael Mühlebach <michael@anduin.ch>
2016-12-11 12:13:18 +0100

    Change default card size for existing monsters in migration script.

--- db/migrate/20160810193307_add_card_size_to_card.rb
+++ db/migrate/20160810193307_add_card_size_to_card.rb
@@ -1,9 +1,14 @@
 class AddCardSizeToCard < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :cards, :card_size, :string, default: '25x35', null: false
 
     Monster.all.each do |monster|
       monster.card_size = '35x50'
+      monster.save!
     end
+  end
+
+  def down
+    remove_column :cards, :card_size
   end
 end
