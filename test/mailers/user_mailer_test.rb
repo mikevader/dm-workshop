142bcc98d7034d6185f500ff90b430c3117b4223
Michael Mühlebach <michael@anduin.ch>
2023-09-25 23:42:24 +0200

    chore: Fix intentional errors

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -18,7 +18,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password asdfasdfasdf reset", mail.subject
+    assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@dnd-cards.com"], mail.from
     assert_match user.reset_token,        mail.body.encoded
