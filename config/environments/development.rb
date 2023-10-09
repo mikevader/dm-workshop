9b15ee2d9646f4b6138657aa1a233cd98d471afb
Michael Mühlebach <michael@anduin.ch>
2023-10-09 07:08:41 +0200

    fix: use other form to configure host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts = [
-    IPAddr.new("0.0.0.0/0"), # All IPv4 addresses.
-    IPAddr.new("::/0"),      # All IPv6 addresses.
-    "localhost",             # The localhost reserved domain.
-    ENV["SERVER_HOST_NAME"]
-  ]
+  config.hosts << ENV["SERVER_HOST_NAME"]
 
   # Settings specified here will take precedence over those in config/application.rb.
 
