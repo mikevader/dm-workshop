8b83c785c5bff8b12d7f58273983442ae8879cde
Michael Mühlebach <michael@anduin.ch>
2023-10-21 21:36:55 +0200

    fix: adapt tests to email changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'dnd-cards.com' }
+  ENV['SMTP_DEFAULT_FROM'] = "noreply@example.com"
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
