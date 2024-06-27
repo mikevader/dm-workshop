83dac12ab2e5f2c26aa0b1a72fdd2ccbefdd85a8
Michael Mühlebach <michael@anduin.ch>
2024-06-27 14:24:52 +0200

    feat: basic construct of rails 7 JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Entry point for the build script in your package.json
-import "./controllers"
-// import "./custom/rails_admin"
+
+console.log('hello1')
+
+import "@hotwired/turbo-rails"
+import "jquery"
+import "jquery_ujs"
+// import "./jquery_ui"
+// import "jquery-ui"
+
+import "popper"
+import "bootstrap"
+// import "bootstrap-sprockets"
+
+import "controllers"
+import "custom/menu"
+import "custom/item-properties"
+
+console.log('hello2')
--- app/javascript/custom/analytics.js
+++ app/javascript/custom/analytics.js
@@ -1,4 +1,6 @@
-$(document).on('page:change', function() {
+console.log("hello")
+document.addEventListener('page:change', function() {
+    console.log("hello")
     if (window._gaq != null) {
         return _gaq.push(['_trackPageview']);
     } else if (window.pageTracker != null) {
