5d6ec4c318bf45575e7ce041bc216d681feeaeb1
Michael Mühlebach <michael@anduin.ch>
2024-06-28 15:13:17 +0200

    fix: admin section

--- app/javascript/custom/admin.js
+++ app/javascript/custom/admin.js
@@ -1,33 +1,15 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require turbolinks
-//= require_self
-
-var selectAll =  function() {
-    $('#selectAll').click(function() {
+const selectAll = function () {
+    document.querySelector('#selectAll').addEventListener('change', function () {
         if (this.checked) {
-            $(':checkbox').each(function() {
-                this.checked = true;
+            document.querySelectorAll('input[type=checkbox]').forEach(function (checkbox) {
+                checkbox.checked = true;
             });
         } else {
-            $(':checkbox').each(function() {
-                this.checked = false;
+            document.querySelectorAll('input[type=checkbox]').forEach(function (checkbox) {
+                checkbox.checked = false;
             });
         }
     });
-}
+};
 
-$(document).on("page:load ready", selectAll);
+document.addEventListener("DOMContentLoaded", selectAll);
