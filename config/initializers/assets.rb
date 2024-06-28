a538d28d4054e13eabb7ab490fd54d7fd26a39c5
Michael Mühlebach <michael@anduin.ch>
2024-06-28 17:00:35 +0200

    fix: linter issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js jquery.min.js jquery_ujs.js jquery-ui.min.js)
-Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js jquery.min.js jquery_ujs.js)
+Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js jquery.min.js jquery_ujs.js]
